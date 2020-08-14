db6a0597ebc1fffbcf928f68c8fec58884767083
Nida.Q <nida@emamilid@gmail.com>
2020-08-14 14:40:51 -0400

    built components as classes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 
+import Loading from 'components/Loading';
 import classnames from "classnames";
 
-class Dashboard extends Component {
+export default class Dashboard extends Component {
+  state = {
+    loading: true
+  };
+
   render() {
     const dashboardClasses = classnames("dashboard");
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
 
     return <main className={dashboardClasses} />;
   }
 }
-
-export default Dashboard;
